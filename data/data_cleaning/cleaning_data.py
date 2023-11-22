fa819f19c307c392a52c6b2ec11abfdf2b50e795
Alexandria Stevenson <stevenson.alex.p@gmail.com>
2023-11-21 22:26:06 -0500

    Updating code in cleaning data + commenting

--- data/data_cleaning/cleaning_data.py
+++ data/data_cleaning/cleaning_data.py
@@ -1,24 +1,27 @@
-import pandas as pd
+import pandas as pd # may have interpreter issues
 import os
 
-df = pd.read_csv(r"data\MBTA Rail Ridership by Time Period, Season, Route_Line, and Stop.csv")
+df = pd.read_csv(r"data\data_cleaning\MBTA Rail Ridership by Time Period, Season, Route_Line, and Stop.csv")
 
-#dropping columns
+#DROPPING UNNECESSARY COLUMNS
 columns_to_drop = ['stop_id', 'day_type_id', 'direction_id', 'mode', 'FID']
 df = df.drop(columns=columns_to_drop)
+# CHANGING VALUES TO BE MORE READ-ABLE
+df['time_period_name'] = df['time_period_name'].str.replace('_', ' ').str.title() #replacing _ with ' ' and recapitalizing
+df['day_type_name'] = df['day_type_name'].str.title() # title casing for weekday/sat/sun
+df['year'] = df['season'].str.split().str[-1].astype(int) # creating a year column
+df['time_period_id'] = df['time_period_id'].str.extract(r'(\d+)').astype(int) # only obtaining integer for id
 
-df['time_period_name'] = df['time_period_name'].str.replace('_', ' ').str.title()
-df['day_type_name'] = df['day_type_name'].str.title()
-df['year'] = df['season'].str.split().str[-1].astype(int)
-df['time_period_id'] = df['time_period_id'].str.extract(r'(\d+)').astype(int)
-
+#RE-CAPITALIZING COLUMN NAMES
 df.columns = [col.replace('_', ' ').title() for col in df.columns]
 
+#EXPORTING DATAFRAME
 directory = "data"
-file_name1 = "MBTA_Data.csv"
-file_name2 = "MBTA_Data.json"
-
-file_path = os.path.join(directory, file_name1)
+file_name_csv = "MBTA_Data.csv"
+file_name_json = "MBTA_Data.json"
+# export as csv
+file_path = os.path.join(directory, file_name_csv)
 df.to_csv(file_path, index=False)
-file_path = os.path.join(directory, file_name2)
+#export as json
+file_path = os.path.join(directory, file_name_json)
 df.to_csv(file_path, index=False)
