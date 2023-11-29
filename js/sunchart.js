5f317dafe5ea062dacba32c221192d4b26c3a1b4
Nistar Steinerman <135656408+ons372@users.noreply.github.com>
2023-11-29 15:13:21 -0500

    add beginnings of linking in visualization.js

--- js/sunchart.js
+++ js/sunchart.js
@@ -10,13 +10,13 @@ var width = 450
 // The radius of the pieplot is half the width or half the height (smallest one). I subtract a bit of margin.
 var radius = Math.min(width, height) / 2 - margin
 
-// append the svg object to the div called 'my_dataviz'
-var svg = d3.select("#sunchart")
+// append the svg object to the div called 'sunchart'
+/*let svg = d3.select("#sunchart")
   .append("svg")
     .attr("width", width)
     .attr("height", height)
   .append("g")
-    .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+    .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");*/
 
 // Create dummy data
 var data = {a: 9, b: 20, c:30, d:8, e:12}
@@ -45,3 +45,9 @@ svg
   .attr("stroke", "black")
   .style("stroke-width", "2px")
   .style("opacity", 0.7)
+
+
+  /* global D3 */
+
+// Initialize a line chart. Modeled after Mike Bostock's
+// Reusable Chart framework https://bost.ocks.org/mike/chart/
--- js/visualization.js
+++ js/visualization.js
@@ -1,7 +1,59 @@
 // Immediately Invoked Function Expression to limit access to our 
-// variables and prevent 
+// variables and prevent race conditions
 ((() => {
 
-  console.log("Hello, world!");
+  // Load the data from a json file (you can make these using
+  // JSON.stringify(YOUR_OBJECT), just remove the surrounding "")
+  d3.json("data/MBTA_data.json", (data) => {
+
+    // General event type for selections, used by d3-dispatch
+    // https://github.com/d3/d3-dispatch
+    const dispatchString = "selectionUpdated";
+
+    // Create a sun chart
+    let sunchart_mbta = sunchart()
+      .selectionDispatcher(d3.dispatch(dispatchString))
+      ("#sunchart", data);
+
+    // Create a ons bar graph chart
+    //let on_bargraph = onchart()
+    //  .selectionDispatcher(d3.dispatch(dispatchString))
+    //  ("#onchart", data);
+
+    // Create a off bar chart
+    //let off_bargraph = offchart()
+    //  .selectionDispatcher(d3.dispatch(dispatchString))
+    //  ("#sunchart", data);
+
+
+
+    // When the sun chart selection is updated via brushing, 
+    // tell the on and off graphs to update it's selection (linking)
+    sunchart_mbta.selectionDispatcher().on(dispatchString, function(selectedData) {
+      //on_bargraph.updateSelection(selectedData);
+      //off_bargraph.updateSelection(selectedData);
+
+    });
+
+        // When the sun chart selection is updated via brushing, 
+    // tell the on and off graphs to update it's selection (linking)
+    //on_bargraph.selectionDispatcher().on(dispatchString, function(selectedData) {
+      //sunchart_mbta.updateSelection(selectedData);
+      //off_bargraph.updateSelection(selectedData);
+
+    //});
+
+        // When the sun chart selection is updated via brushing, 
+    // tell the on and off graphs to update it's selection (linking)
+    //off_bargraph.selectionDispatcher().on(dispatchString, function(selectedData) {
+      //on_bargraph.updateSelection(selectedData);
+      //sunchart.updateSelection(selectedData);
+
+    //});
+
+
+
+
+  });
 
 })());
